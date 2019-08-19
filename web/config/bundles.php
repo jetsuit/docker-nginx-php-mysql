408da2858ae3fbefd846d9e2abe645518f195447
Serg Dudchenko <jetsuit@gmail.com>
2019-08-19 23:08:44 +0300

    Gesident refresh token bundle added

--- web/config/bundles.php
+++ web/config/bundles.php
@@ -16,4 +16,5 @@ return [
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle::class => ['all' => true],
 ];
